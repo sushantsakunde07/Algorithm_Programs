79f84ea7f6301e166cc6642aa643bb91f7c85f82
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-05-26 06:47:51 +0530

    Added: Searching prime number that are Anagram and Palindrome

--- src/main/java/com/bridgelabz/SearchingAndSorting.java
+++ src/main/java/com/bridgelabz/SearchingAndSorting.java
@@ -1,41 +1,68 @@
 package com.bridgelabz;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SearchingAndSorting {
 
-    private static void isPrime(int lower, int upper) {
-        int i, flag, j;
-        for (i = lower; i <= upper; i++) {
-            if (i == 1 || i == 0)
-                continue;
-            flag = 1;
-            for (j = 2; j <= i / 2; ++j) {
-                if (i % j == 0) {
-                    flag = 0;
-                    break;
-                }
+    // check number is prime or not
+    public static boolean checkPrime(int num) {
+        // zero and one is not prime number
+        if (num == 0 || num == 1) {
+            return false;
+        }
+
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
             }
-            if (flag == 1)
-                System.out.println(i);
+        }
+        return true;
+    }
+
+    // check number is palindrome or not
+    public static boolean checkPalindrome(int num) {
+        int r, sum = 0;
+        int temp = num;
+        while (num > 0) {
+            r = num % 10;
+            sum = (sum * 10) + r;
+            num = num / 10;
+        }
+        if (temp == sum) {
+            return true;
+        } else {
+            return false;
         }
     }
 
+    // check number is anagram or not
+    public static boolean checkAnagram(String str1, String str2) {
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        if (str1.length() != str2.length()) {
+            return false;
+        } else {
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+            if (Arrays.equals(arr1, arr2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        System.out.println("-------------Welcome to Prime No Program----------");
-        System.out.println();
-
-        Scanner sc = new Scanner(System.in);
-        int lower, upper;
-
-        // Taking limit from the user
-        System.out.print("Enter the lower bound : ");
-        lower = sc.nextInt();
-        System.out.print("Enter the upper bound : ");
-        upper = sc.nextInt();
-        System.out.println("Prime numbers between " + lower + " and " + upper + " are : ");
-
-        // Calling isPrime method
-        isPrime(lower, upper);
+        System.out.println(" Prime Numbers that are Anagram and pallindrome between 0 to 1000 : ");
+        for (int i = 0; i <= 1000; i++) {
+            // check number is prime as well as palindrome or not
+            if (checkPrime(i) == true && checkPalindrome(i)) {
+                // check number is anagram or not
+                boolean result = checkAnagram(String.valueOf(i), String.valueOf(i));
+                if (result == true) {
+                    System.out.println(" " + i);
+                }
+            }
+        }
     }
 }
